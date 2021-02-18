98141e5d55e6f97e2c76b993353e3f754531b27f
Luis <luissoto9@gmail.com>
2021-02-18 10:26:57 -0300

    arreglo .bat

---
+++ start.bat
@@ -0,0 +1,5 @@
+SET JAVA_HOME=C:\Program Files (x86)\IBM\Notes\jvm
+SET Path=C:\Program Files (x86)\IBM\Notes;C:\software\apache-maven-3.6.3\bin;C:\Program Files (x86)\IBM\Notes\jvm\bin
+REM t:
+REM cd T:\git\domino-springboot\domino-springboot.plugin
+mvn spring-boot:run
